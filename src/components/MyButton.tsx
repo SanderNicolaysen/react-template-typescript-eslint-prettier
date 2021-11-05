2eedd985866d0774795c7d5cc494a66c8abfc62d
Sander Nicolaysen <sander.nicolaysen@tietoevry.com>
2021-11-05 21:29:24 +0100

    Initial commit

---
+++ src/components/MyButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyButton = (): JSX.Element => {
+    return (
+        <button className="button" type="submit">
+            Hello
+        </button>
+    )
+}
+
+export default MyButton
